8a14dedaa0fb488a68bfc8eac91aed8efd4013e8
Pratik Shinde <pratikshinde9957@gmail.com>
2026-05-11 03:39:08 +0530

    fix url

--- src/componants/Window.jsx
+++ src/componants/Window.jsx
@@ -26,7 +26,7 @@ function Window({ videoUrl, isHost, roomId }) {
 
     if (Hls.isSupported()) {
       const hls = new Hls()
-      hls.loadSource(`${process.env.REACT_APP_SOCKET_URL}${videoUrl}`)
+      hls.loadSource(`${process.env.VITE_SOCKET_URL}${videoUrl}`)
       hls.attachMedia(video)
     }
 
--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.REACT_APP_SOCKET_URL, {
+export const socket = io(process.env.VITE_SOCKET_URL, {
   withCredentials: true,
 });
-
+console.log("🔌 Attempting to connect to socket server at:", process.env.VITE_SOCKET_URL);
 socket.on("connect", () => {
   console.log("🔌 Socket connected to server:", socket.id);
 });
